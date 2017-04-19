9a89d3f86e6fbfd5275b74e74b6fc4255b718e38
sametaylak <sametaylak29@gmail.com>
2017-04-19 12:50:09 +0300

    Sign tasarımı için geçişler yazıldı

--- app/src/main/java/com/gelecegiyazanlar/hocamnerede/MainActivity.java
+++ app/src/main/java/com/gelecegiyazanlar/hocamnerede/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gelecegiyazanlar.hocamnerede;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
     }
 }
