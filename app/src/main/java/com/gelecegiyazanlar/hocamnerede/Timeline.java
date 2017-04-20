fee5c9e1062c6fa596982f584e5b2922dfc0f7c7
özenç çelik <zen elik>
2017-04-20 12:35:11 +0300

    Hatalı Location Fixlenecek

--- app/src/main/java/com/gelecegiyazanlar/hocamnerede/Timeline.java
+++ app/src/main/java/com/gelecegiyazanlar/hocamnerede/Timeline.java
@@ -1,12 +1,10 @@
 package com.gelecegiyazanlar.hocamnerede;
 
-import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
@@ -18,17 +16,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+
+//import com.google.android.gms.location.LocationListener;
 
 
-public class Timeline extends Fragment implements View.OnClickListener,GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+public class Timeline extends Fragment {
 
     private FloatingActionButton sendLocation;
-    private TextView text;
 
     private TextView txtOutputLatLon;
     private Location mLastLocation;
@@ -39,139 +35,119 @@ public class Timeline extends Fragment implements View.OnClickListener,GoogleApi
     float lon;
     View mView;
 
-    public Timeline(){
+    public Timeline() {
 
     }
 
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState)
-    {
+    public void onSaveInstanceState(Bundle savedInstanceState) {
         //LOGS SHOW THAT THIS IS ALWAYS CALLED WITH CORRECT VALUES
         super.onSaveInstanceState(savedInstanceState);
         /*
         savedInstanceState.putString("string", "example");
         savedInstanceState.putBoolean("boolean", bool);*/
     }
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Toast.makeText(getActivity(), "DEğiştiiii", Toast.LENGTH_SHORT).show();
+            Log.d("Location Changed", "Lat : " + location.getLatitude());
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Toast.makeText(getActivity(), "status mü", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getActivity(), "Açıldıııı", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getActivity(), "Kapandııı", Toast.LENGTH_SHORT).show();
+        }
+
+
+    };
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,final Bundle savedInstanceState) {
-
-
-        mView = inflater.inflate(R.layout.pop_up_message, container, false);
-
-        txtOutputLatLon = (TextView) mView.findViewById(R.id.text);
-        buildGoogleApiClient();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
 
 
 
-        text = (TextView)mView.findViewById(R.id.text);
-        sendLocation = (FloatingActionButton)mView.findViewById(R.id.floatingActionButton);
+
+
+
+
+
+        mView = inflater.inflate(R.layout.fragment_timeline, container, false);
+
+
+        sendLocation = (FloatingActionButton) mView.findViewById(R.id.floatingActionButton);
 
         sendLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                ////////////////////////////////////////////////
-                mLocationRequest = LocationRequest.create();
-                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                mLocationRequest.setInterval(100); // Update location every second
+               final LocationManager locationManager = (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
 
-                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(mView.getContext(), "İzin alınamadı....", Toast.LENGTH_SHORT).show();
-                    return;
+
+
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+                }
+                else{
+                    Toast.makeText(getActivity(),"Konum Servisiniz Kapalı",Toast.LENGTH_SHORT).show();
                 }
 
 
-                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                if (mLastLocation != null) {
-                    lat = (float) mLastLocation.getLatitude();
-                    lon = (float) mLastLocation.getLongitude();
 
-                    Toast.makeText(mView.getContext(),lat+" "+lon, Toast.LENGTH_LONG).show();
 
-                }
-                updateUI();
-                ////////////////////////////////////////////////
-                AlertDialog.Builder mBuild = new AlertDialog.Builder(v.getContext());
 
-                View view = LayoutInflater.from(getActivity()).inflate(R.layout.pop_up_message, null);
-                final EditText mMessage = (EditText)getActivity().findViewById(R.id.editText);
 
-                Button sendMessage = (Button)getActivity().findViewById(R.id.sendButton);
+                //Eğer Konum Servisi Açıksa Pop Up Göstersin
+                    final AlertDialog.Builder mBuild = new AlertDialog.Builder(v.getContext());
 
-                sendMessage.setOnClickListener(new View.OnClickListener() {
-                    @Override
+                    final View view = LayoutInflater.from(getActivity()).inflate(R.layout.pop_up_message, null);
+                    final EditText mMessage = (EditText) view.findViewById(R.id.editText);
+
+
+                    txtOutputLatLon = (TextView) view.findViewById(R.id.text);
+
+                    mBuild.setView(view);
+                    final AlertDialog dialog = mBuild.create();
+
+                    dialog.show();
+                    Button sendMessage = (Button) view.findViewById(R.id.sendButton);
+
+                    sendMessage.setOnClickListener(new View.OnClickListener() {
+                        @Override
                         public void onClick(View v) {
-                            if(!mMessage.getText().toString().isEmpty()){
+                            if (!mMessage.getText().toString().isEmpty()) {
 
                                 mMessage.setText("");
-
-
+                                getActivity().onBackPressed();
+                                //view.setPressed(true);
+                                dialog.dismiss();
                             }
-                    }
-                });
-                mBuild.setView(view);
-                AlertDialog dialog = mBuild.create();
-                dialog.show();
+                        }
+                    });
+
+
             }
         });
         return mView;
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
 
-    }
 
-    @Override
-    public void onConnectionSuspended(int i) {
-        buildGoogleApiClient();
-    }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        lat = (float) location.getLatitude();
-        lon = (float) location.getLongitude();
-        updateUI();
-    }
-
-    synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mGoogleApiClient.connect();
-        Log.v("GEldim","heyyyy");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Toast.makeText(getContext(),"niden olmuyon",Toast.LENGTH_SHORT).show();
-        mGoogleApiClient.connect();
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mGoogleApiClient.disconnect();
-    }
-
-    void updateUI() {
-        txtOutputLatLon.setText(lat+"\n"+lon);
-    }
-    @Override
-    public void onClick(View v) {
-        text.setText("Girdi 2");
-    }
 }
